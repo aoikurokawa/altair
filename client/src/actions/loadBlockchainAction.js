9777e3ec5320e76da0af1b5be27b75063a7ced66
Aoi <ht.so.ik0102@gmail.com>
2021-07-03 16:54:39 +0700

    fix bug in loadBlockchainAction

--- client/src/actions/loadBlockchainAction.js
+++ client/src/actions/loadBlockchainAction.js
@@ -5,11 +5,11 @@ export const loadBlockchain = () => async (dispatch) => {
     const web3 = new Web3(Web3.givenProvider || "http:localhost:8545");
     const accounts = await web3.eth.getAccounts();
     const selectedAccounts = window.ethereum.selectedAddress;
-    const contractInstance = new web3.eth.Contract(AUCTIONABI, AUCTIONADDRESS, {from: accounts[0]});
+    const contractInstance = new web3.eth.Contract(AUCTIONABI, AUCTIONADDRESS, { from: accounts[0] });
 
     dispatch({
-        type: "GET_CONTRACTDATA", 
-        contractInstance: contractInstance, 
+        type: "GET_CONTRACTDATA",
+        contractInstance: contractInstance,
         accounts: accounts,
     });
     contractInstance.methods
@@ -17,7 +17,7 @@ export const loadBlockchain = () => async (dispatch) => {
         .call()
         .then((result) => {
             dispatch({
-                type: "GET_HIGHESTBID", 
+                type: "GET_HIGHESTBID",
                 highestBid: Web3.utils.fromWei(result, "ether"),
             });
         });
@@ -26,13 +26,39 @@ export const loadBlockchain = () => async (dispatch) => {
         .auctionTimeEnd()
         .call()
         .then((res) => {
-            
+
             let endDate = new Date(res * 1);
             let auctionTime = endDate.toDateString();
             dispatch({
-                type: "GET_AUCTIONTIMEEND", 
+                type: "GET_AUCTIONTIMEEND",
                 auctionTimeEnd: auctionTime,
             });
         });
+};
+
+export const bidHandlerAction = (price) => async (dispatch) => {
+    const web3 = new Web3(Web3.givenProvider || "http:localhost:8545");
+    const accounts = await web3.eth.getAccounts();
+    const selectedAccounts = window.ethereum.selectedAddress;
+    const contractInstance = new web3.eth.Contract(AUCTIONABI, AUCTIONADDRESS, { from: accounts[0] });
+
+    if (price <= 0) {
+        alert("Please put more than 0");
+    } else {
+        let config = {
+            value: Web3.utils.toWei(price, "ether"),
+            from: accounts[0],
+        };
+        contractInstance.methods
+            .bid()
+            .send(config)
+            .on("transactionHash", (hash) => {
+                console.log(hash);
+            })
+            .on("receipt", (receipt) => {
+                console.log(receipt);
+                alert("Congratulations!");
+            });
+    }
 };
 
--- client/src/components/BidModal.js
+++ client/src/components/BidModal.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Web3 from 'web3';
 import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core';
 import { Modal } from '@material-ui/core';
@@ -10,6 +9,7 @@ import { Container } from '@material-ui/core';
 import { Grid } from '@material-ui/core';
 
 import { updateIsSell } from '../actions/artTokenAction';
+import { bidHandlerAction } from '../actions/loadBlockchainAction';
 
 const useStyles = makeStyles((theme) => ({
     modal: {
@@ -46,10 +46,9 @@ const useStyles = makeStyles((theme) => ({
 const BidModal = () => {
     const [price, setPrice] = useState(0);
     const classes = useStyles();
-    const { contractInstance, accounts } = useSelector((state) => state.loadBlockchain);
     const { isModalVisible, functionType, title, objectId } = useSelector((state) => state.modal);
     const dispatch = useDispatch();
-    
+
 
     const handleClose = () => {
         dispatch({
@@ -58,79 +57,62 @@ const BidModal = () => {
     };
 
     const bidHandler = () => {
-        if (price <= 0) {
-            alert("Please put more than 0");
-        }
-
-        let config = {
-            value: Web3.utils.toWei(price, "ether"),
-            from: accounts[0],
-        };
-        contractInstance.methods
-            .bid()
-            .send(config)
-            .on("transactionHash", (hash) => {
-                console.log(hash);
-            })
-            .on("receipt", (receipt) => {
-                console.log(receipt);
-                alert("Congratulations!");
-            });
+        dispatch(bidHandlerAction(price));
         dispatch({
             type: 'CLOSE_MODAL'
-        })
+        });
     }
 
     const handleAuction = () => {
         dispatch(updateIsSell(objectId));
     }
 
-return (
-    <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={isModalVisible}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-            timeout: 500,
-        }}
-    >
-        <Fade in={isModalVisible}>
-            <div className={classes.paper}>
-                <h3 id="transition-modal-title">{title}</h3>
-                {functionType === "Bid" &&
-                    <>
-                        <div className={classes.inputDiv}>
-                            <input type="number" className={classes.input} onChange={(e) => setPrice(e.target.value)} value={price} />
-                            <p>ETH</p>
-                        </div>
-                        <Container component="div" maxWidth="lg" className={classes.buttonContainer}>
-                            <Button variant="contained" color="primary" disableElevation onClick={bidHandler}>BID</Button>
-                        </Container>
-                        <p id="transition-modal-description">react-transition-group animates me.</p>
-                    </>
-                }
-                {functionType === "MyPage" &&
-                    <Grid container spacing={3} alignItems="center" style={{ height: "100%", textAlign: "center" }}>
-                        <Grid item xs={6}>
-                            <Button variant="outlined" color="primary" onClick={handleAuction}>
-                                OK
-                            </Button>
+    return (
+        <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={isModalVisible}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+                timeout: 500,
+            }}
+        >
+            <Fade in={isModalVisible}>
+                <div className={classes.paper}>
+                    <h3 id="transition-modal-title">{title}</h3>
+                    {functionType === "Bid" &&
+                        <>
+                            <div className={classes.inputDiv}>
+                                <input type="number" className={classes.input} onChange={(e) => setPrice(e.target.value)} value={price} />
+                                <p>ETH</p>
+                            </div>
+                            <Container component="div" maxWidth="lg" className={classes.buttonContainer}>
+                                <Button variant="contained" color="primary" disableElevation onClick={() => bidHandler()}>BID</Button>
+                            </Container>
+                            <p id="transition-modal-description">react-transition-group animates me.</p>
+                        </>
+                    }
+                    {functionType === "MyPage" &&
+                        <Grid container spacing={3} alignItems="center" style={{ height: "100%", textAlign: "center" }}>
+                            <Grid item xs={6}>
+                                <Button variant="outlined" color="primary" onClick={handleAuction}>
+                                    OK
+                                </Button>
+                            </Grid>
+                            <Grid item xs={6}>
+                                <Button variant="outlined" color="secondary" onClick={handleClose}>
+                                    CANCEL
+                                </Button>
+                            </Grid>
                         </Grid>
-                        <Grid item xs={6}>
-                            <Button variant="outlined" color="secondary" onClick={handleClose}>
-                                CANCEL
-                            </Button>
-                        </Grid>
-                    </Grid>
-                }
-            </div>
-        </Fade>
-    </Modal>
-);
+                    }
+                </div>
+            </Fade>
+        </Modal>
+    );
 }
 
 export default BidModal;
