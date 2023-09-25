6b6109b18390da4fb112636efce4b483e48a475f
aoikurokawa <ht.so.ik0102@gmail.com>
2023-09-26 08:09:02 +0900

    feat: make a request with reqwest

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 use futures::future::join_all;
-use reqwest;
+use reqwest::{self, Client, Url};
 use std::time::Duration;
 use tokio::time::sleep;
 
 #[tokio::main]
 async fn main() {
-    // loop {
-    //let mut handles = vec![];
+    let base_url = "http://127.0.0.1:8001/eth/v1/beacon/states/head/validators";
+    let client = Client::new();
+
     let mut pub_keys = String::new();
     for _ in 0..1023 {
         pub_keys.push_str("0xb89bebc699769726a318c8e9971bd3171297c61aea4a6578a7a4f94b547dcba5bac16a89108b6b6a1fe3695d1a874a0b,");
     }
 
-    let url = format!(
-        "http://127.0.0.1:8001/eth/v1/beacon/states/head/validators?id={}",
-        pub_keys
-    );
-    // println!("{}", url);
-    let res = reqwest::get(url).await.unwrap();
+    let params = [("id", pub_keys)];
+
+    let res = client
+        .get(Url::parse(base_url).unwrap())
+        .query(&params)
+        .send()
+        .await
+        .unwrap();
 
     println!("{:?}", res.text().await.unwrap());
 }
