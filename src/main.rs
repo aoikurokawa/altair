65cf54f3083f486de54c8a42b0719dedcfe9b9fa
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-01 08:37:56 +0900

    feat: error: too long uri

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,16 @@ use tokio::time::sleep;
 async fn main() {
     let base_url = "http://127.0.0.1:8001/eth/v1/beacon/states/head/validators";
     let client = Client::new();
+    let range: Vec<u16> = (0..699).collect();
+    let last = range.last().unwrap();
 
     let mut pub_keys = String::new();
-    for _ in 0..1023 {
-        pub_keys.push_str("0xb89bebc699769726a318c8e9971bd3171297c61aea4a6578a7a4f94b547dcba5bac16a89108b6b6a1fe3695d1a874a0b,");
+    for val in range.clone() {
+        if val == *last {
+            pub_keys.push_str("0xb89bebc699769726a318c8e9971bd3171297c61aea4a6578a7a4f94b547dcba5bac16a89108b6b6a1fe3695d1a874a0b");
+        } else {
+            pub_keys.push_str("0xb89bebc699769726a318c8e9971bd3171297c61aea4a6578a7a4f94b547dcba5bac16a89108b6b6a1fe3695d1a874a0b,");
+        }
     }
 
     // let params = [("id", pub_keys)];
@@ -24,9 +30,7 @@ async fn main() {
     let url = format!("{}?id={}", base_url, pub_keys);
     println!("url: {}", url);
 
-    let res = reqwest::get(url)
-        .await
-        .unwrap();
+    let res = reqwest::get(url).await.unwrap();
 
     println!("{:?}", res.text().await.unwrap());
 }
