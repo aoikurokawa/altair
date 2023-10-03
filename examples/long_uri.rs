41df7db658eef00ad2458fabad5939ce4a39d715
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-04 07:44:33 +0900

    feat: compiling now

--- examples/long_uri.rs
+++ examples/long_uri.rs
@@ -1,8 +1,5 @@
-use std::net::SocketAddr;
-
 use reqwest::StatusCode;
-use serde::Deserialize;
-use warp::{http::Response, path::FullPath, Filter, Rejection, Reply};
+use warp::{Filter, Rejection, Reply};
 
 #[derive(Debug)]
 struct UriTooLong;
@@ -14,17 +11,15 @@ struct FooParamError;
 impl warp::reject::Reject for FooParamError {}
 
 fn uri_length_limit(limit: usize) -> impl Filter<Extract = ((),), Error = warp::Rejection> + Clone {
-    warp::any()
-        .and(warp::filters::addr::remote())
-        .and_then(move |addr: Option<SocketAddr>| {
-            if let Some(a) = addr {
-                let length = a.ip().to_string().len();
-                if length > limit {
-                    return futures::future::err(warp::reject::custom(UriTooLong));
-                }
+    warp::any().and(warp::path::full()).and_then(
+        move |route: warp::filters::path::FullPath| async move {
+            if route.as_str().len() > limit {
+                Err(warp::reject::custom(UriTooLong))
+            } else {
+                Ok(())
             }
-            futures::future::ok(())
-        })
+        },
+    )
 }
 
 async fn handle_rejection(err: Rejection) -> Result<impl Reply, std::convert::Infallible> {
@@ -51,6 +46,8 @@ fn hello_filter() -> impl Filter<Extract = (impl Reply,), Error = warp::Rejectio
 async fn main() {
     let routes = uri_length_limit(10)
         .and(hello_filter())
+        .map(|_, reply| reply)
         .recover(handle_rejection);
+
     warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
 }
