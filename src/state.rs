1c3be0281783c62fa26b3d5f68cfaec8354610c4
aoikurokawa <ht.so.ik0102@gmail.com>
2024-01-22 08:29:06 +0900

    feat: init new project and declare state.rs

---
+++ src/state.rs
@@ -0,0 +1,56 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    program_pack::{IsInitialized, Sealed},
+    pubkey::Pubkey,
+};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieAccountState {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub reviewer: Pubkey,
+    pub rating: u8,
+    pub title: String,
+    pub description: String,
+}
+
+impl Sealed for MovieAccountState {}
+
+impl IsInitialized for MovieAccountState {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieCommentCounter {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub counter: u64,
+}
+
+impl Sealed for MovieCommentCounter {}
+
+impl IsInitialized for MovieCommentCounter {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+pub struct MovieComment {
+    pub discriminator: String,
+    pub is_initialized: bool,
+    pub review: Pubkey,
+    pub commenter: Pubkey,
+    pub comment: String,
+    pub count: u64,
+}
+
+impl Sealed for MovieComment {}
+
+impl IsInitialized for MovieComment {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
